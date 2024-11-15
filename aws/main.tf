1c1fd09a2426b35c14fe9b6c9ee46af867e74abc
geri <geri.mujo@softup.co>
2024-11-15 15:19:50 +0100

    show ecr repo

--- aws/main.tf
+++ aws/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraformRemoteStateGeri"
+    bucket = "terraform-remote-state-geri"
     key    = "key/terraform.tfstate"
     region = "eu-central-1"
   }
