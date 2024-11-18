c3ab7671abe857d81a1e68a8003f12b39c4fa35f
geri <geri.mujo@softup.co>
2024-11-18 13:08:17 +0100

    final commit

--- web_client/src/config.js
+++ web_client/src/config.js
@@ -1,5 +1,7 @@
 const config = {
-  SERVER_URL: process.env.REACT_APP_SERVER_URL || "http://geri-lb-1899535009.eu-central-1.elb.amazonaws.com",
+  SERVER_URL:
+    process.env.REACT_APP_SERVER_URL ||
+    "http://geri-lb-47103475.eu-central-1.elb.amazonaws.com",
 };
 
 export default config;
